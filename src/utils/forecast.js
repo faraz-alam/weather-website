ee7b96aa37a697e7cc2a3babfffb582b5dade7be
Faraz Alam <faraz.alam134@gmail.com>
2019-10-03 02:21:14 +0530

    Add max & min temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location or ' +body.error, undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary +' It is currently '+ body.currently.temperature +' degrees out. There is a '+body.currently.precipProbability +'% chance of rain.')
+            callback(undefined, body.daily.data[0].summary +' It is currently '+ body.currently.temperature +' degrees out. There is a '+body.currently.precipProbability +' probability of rain. The maximum and minimum temperature for today is ' +body.daily.data[0].temperatureHigh+' and '+body.daily.data[0].temperatureLow+' respectively.')
         }
     })
 }
